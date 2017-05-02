eeebb9fb6cafd11b9418c86327318438130caaba
Adam Helinski <helinski.adam@outlook.com>
2017-05-02 17:05:41 +0200

    Modify API semantics and fix sending records
    Names and signatures have been improved. Notably, consumers and
    producers are created via a completely new way.
    
    Sending records was misbehaving when the partition was missing.

--- project.clj
+++ project.clj
@@ -7,15 +7,19 @@
   :plugins      [[lein-codox "0.10.3"]]
   :codox        {:output-path "doc/auto"
                  :namespaces  [#"^milena"]}
-  :main         milena.core
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
-                 [org.apache.kafka/kafka-clients "0.10.2.0"]
-                 ]
+  :dependencies [[org.apache.kafka/kafka-clients "0.10.2.0"]]
   :profiles     {:dev     {:source-paths ["dev"]
                            :main         user
-                           :plugins      [[venantius/ultra "0.4.1"]                ;; colorful repl
+                           :plugins      [[mvxcvi/whidbey "1.3.1"]]
+                           :whidbey      {:color-scheme {:keyword         [:green]
+                                                         :boolean         [:bold :yellow]
+                                                         :character       [:white]
+                                                         :string          [:white]
+                                                         :symbol          [:bold :magenta]
+                                                         :nil             [:bold :cyan]}}
+                           :dependencies [[org.clojure/clojure         "1.9.0-alpha10"]
+                                          [org.clojure/tools.namespace "0.2.11"]
+                                          [criterium                   "0.4.4"]
+                                          ;[org.clojure/test.check     "0.9.0"]]
                                           ]
-                           :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                          [criterium "0.4.4"]
-                                          ;[org.clojure/test.check "0.9.0"]]
-                                          ]}})
+                           :global-vars  {*warn-on-reflection* true}}})
