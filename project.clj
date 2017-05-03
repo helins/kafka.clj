5ad00fab6396e5b52d28505197e89f6c1f6f9e58
Adam Helinski <helinski.adam@outlook.com>
2017-05-03 11:11:12 +0200

    Fixed examples

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject dvlopt/milena
-            "0.0.0-alpha2"
+            "0.0.0-alpha3"
   :description  "A straightforward clojure client for Kafka"
   :url          "https://github.com/dvlopt/milena"
   :license      {:name "Eclipse Public License"
