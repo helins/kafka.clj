7b441b4c5651da9c56807e171393201de119ed89
Adam Helinski <helinski.adam@outlook.com>
2017-05-06 16:47:42 +0200

    Improve README

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject dvlopt/milena
-            "0.0.0-alpha3"
+            "0.0.0-alpha4"
   :description  "A straightforward clojure client for Kafka"
   :url          "https://github.com/dvlopt/milena"
   :license      {:name "Eclipse Public License"
